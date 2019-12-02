90b9021c8c8b206013c87421d841686c50230676
Bernhard Schmid <bernhard.schmid@de.ibm.com>
2019-12-02 13:57:07 +0100

    Upload default stemcell from manifest
    [bedrock/mrr-release#334]
    
    Signed-off-by: Tobias Zipfel <tobias.zipfel@de.ibm.com>

--- tasks/bosh-deploy.sh
+++ tasks/bosh-deploy.sh
@@ -3,4 +3,6 @@
 . 1-click/tasks/bosh-login.sh
 
 bosh2 upload-stemcell $(cat stemcell/source)
+MANIFEST_DEFAULT_STEMCELL=$(bosh interpolate state/environments/softlayer/director/$BOSH_LITE_NAME/cf-deployment/manifest.yml --path=/stemcells/alias=default/version)
+bosh2 upload-stemcell https://bosh.io/d/stemcells/bosh-warden-boshlite-ubuntu-xenial-go_agent?v=$MANIFEST_DEFAULT_STEMCELL
 bosh2 -n -d cf deploy state/environments/softlayer/director/$BOSH_LITE_NAME/cf-deployment/manifest.yml
